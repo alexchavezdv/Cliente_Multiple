3ab64c8a1003605744f2c7136c1cb3913b6e1d4f
alexchavezdv <a216213989@unison.mx>
2023-11-27 16:47:43 -0700

    Cliente  para conectarse al servidor

---
+++ src/cliente_multiple/ClientThread.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente_multiple;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author alexande chavez
+ */
+public class ClientThread extends Thread {
+    private Socket socket;
+    private BufferedReader input;
+    public ClientThread(Socket socket) throws IOException {
+        this.socket = socket;
+        this.input = new BufferedReader ( 
+                new InputStreamReader(socket.getInputStream()));
+    }
+    
+    @Override
+    public void run() {
+        try {
+            while(true) {
+                String response = input.readLine();
+                System.out.println(response);
+            }
+        } catch (IOException e) {
+            System.out.println("ClientThread error:"+e.getMessage());
+        } finally {
+            try {
+                input.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
---
+++ src/cliente_multiple/Cliente_multiple.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente_multiple;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexander chavez
+ */
+public class Cliente_multiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("10.10.184.70",5000)){
+            BufferedReader input = new BufferedReader( new 
+                InputStreamReader(socket.getInputStream()));
+            PrintWriter output = 
+                    new PrintWriter(socket.getOutputStream(),true);
+            Scanner scanner = new Scanner(System.in);
+            String userInput;
+            String response;
+            String clientName = "anonymous";
+            ClientThread clientThread = new ClientThread(socket);
+            clientThread.start();
+            do {
+                if (clientName.equals("anonymous")) {
+                    System.out.print("Escribe tu nombre>:");
+                    userInput = scanner.nextLine();
+                    clientName = userInput;
+                    output.println(userInput);
+                    if (userInput.equals("salir")) {
+                        break;
+                    }
+               } else {
+                    String message = ( "("+clientName+")"+" mensaje: " );
+                    System.out.println(message);
+                    userInput = scanner.nextLine();
+                    // output.println(message + " " + userInput);
+                    output.println(userInput);
+                    if (userInput.equals("salir")) {
+                        break;
+                    }
+                }
+            } while (!userInput.equals("salir"));
+        } catch (Exception e) {
+            System.out.println("Main client. Error:"+e.getMessage());
+        }
+    }
+    
+}
